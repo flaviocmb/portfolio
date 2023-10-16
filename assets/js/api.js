9461f96966d67fe75ef1c1b991ece37cd38d338b
flaviocmb <flavio.cmb@gmail.com>
2023-10-16 14:03:47 -0300

    url da API alterada

--- assets/js/api.js
+++ assets/js/api.js
@@ -8,7 +8,7 @@
 async function fetchProfileData() {
 
     // const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
-    const url = 'https://raw.githubusercontent.com/flaviocmb/codigos-javascript/main/19%20-%20Portf%C3%B3lio%20Profissional%20(CSS%20e%20JS)/assets/data/profile.json?token=GHSAT0AAAAAACH5U27NY4XYTB7CZIHTTL3UZJNDBJQ';
+    const url = 'https://raw.githubusercontent.com/flaviocmb/portfolio/main/assets/data/profile.json';
     const response = await fetch(url);
     const profileData = await response.json();
     return profileData;
