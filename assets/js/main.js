4577369badeb0a83db0bbf4efb5e83e528960aea
flaviocmb <flavio.cmb@gmail.com>
2023-10-17 20:11:46 -0300

    profile.json atualizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,39 @@
  * 
  */
 
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo');
+    // photo.src = profileData.photo;
+    photo.alt = profileData.name;
+
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job');
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location');
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = profileData.phone
+
+    const email = document.getElementById('profile.email');
+    email.innerText = profileData.email
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softskills');
+    console.log(typeof softSkills);
+
+    softSkills.innerHTML = profileData.skills.softSkills.map((skill => `<li>${skill}</li>`)).join('');
+}
+
 //função imediatamente invocada
 (async () => {
 
     const profileData = await fetchProfileData()
-    console.log(profileData);
-    
+    updateProfileInfo(profileData);
+    updateSoftSkills(profileData);   
 
 })()
