c28deb42af0df00553212c615e2378bc954dd03a
flaviocmb <flavio.cmb@gmail.com>
2023-10-19 00:28:30 -0300

    profile.json Experiência Profissional atualizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,76 @@
 /**
- * 
+ *
  * Gerenciar o HTML
- * 
- * 
+ *
+ *
  */
 
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo');
-    // photo.src = profileData.photo;
-    photo.alt = profileData.name;
+  const photo = document.getElementById("profile.photo");
+  // photo.src = profileData.photo;
+  photo.alt = profileData.name;
 
-    const name = document.getElementById('profile.name');
-    name.innerText = profileData.name
+  const name = document.getElementById("profile.name");
+  name.innerText = profileData.name;
 
-    const job = document.getElementById('profile.job');
-    job.innerText = profileData.job
+  const job = document.getElementById("profile.job");
+  job.innerText = profileData.job;
 
-    const location = document.getElementById('profile.location');
-    location.innerText = profileData.location
+  const location = document.getElementById("profile.location");
+  location.innerText = profileData.location;
 
-    const phone = document.getElementById('profile.phone');
-    phone.innerText = profileData.phone
+  const phone = document.getElementById("profile.phone");
+  phone.innerText = profileData.phone;
 
-    const email = document.getElementById('profile.email');
-    email.innerText = profileData.email
+  const email = document.getElementById("profile.email");
+  email.innerText = profileData.email;
 }
 
 function updateSoftSkills(profileData) {
-    const softSkills = document.getElementById('profile.skills.softskills');
-    softSkills.innerHTML = profileData.skills.softSkills.map((skill => `<li>${skill}</li>`)).join('');
+  const softSkills = document.getElementById("profile.skills.softskills");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
 }
 
 function updateHardSkills(profileData) {
-    const hardSkills = document.getElementById('profile.skills.hardskills');
-    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => 
-        `<li><img src="${skill.src}" alt="${skill.name}" title="${skill.name}"></li>`).join('');
+  const hardSkills = document.getElementById("profile.skills.hardskills");
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(
+      (skill) =>
+        `<li><img src="${skill.src}" alt="${skill.name}" title="${skill.name}"></li>`
+    )
+    .join("");
 }
 
-function updateLanguages (profileData) {
-    const languages = document.getElementById('languages');
-    languages.innerHTML = profileData.languages.map(language => `<li>${language}`).join('');
+function updateLanguages(profileData) {
+  const languages = document.getElementById("profile.languages");
+  languages.innerHTML = profileData.languages
+    .map((language) => `<li>${language}`)
+    .join("");
+}
+
+function updatePortfolio(profileData) {
+  const portfolio = document.getElementById("profile.portfolio");
+  portfolio.innerHTML = profileData.portfolio
+    .map((project) => {
+      return `
+            <li>
+                <span class="portfolio-title ${
+                  project.github ? 'github' : ""
+                }">${project.name}</span>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `;
+    }).join('');
 }
 
 //função imediatamente invocada
 (async () => {
-
-    const profileData = await fetchProfileData()
-    updateProfileInfo(profileData);
-    updateSoftSkills(profileData);   
-    updateHardSkills(profileData);
-    updateLanguages(profileData);
-
-})()
+  const profileData = await fetchProfileData();
+  updateProfileInfo(profileData);
+  updateSoftSkills(profileData);
+  updateHardSkills(profileData);
+  updateLanguages(profileData);
+  updatePortfolio(profileData);
+})();
